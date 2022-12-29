3fb1549d0961433dcdb9e0753cf91eb3960a06d1
Andres Mauricio Rubiano <andres.rubiano@gascocolombia.corp>
2022-12-29 14:53:38 -0500

    change data enviroment

--- environments.py
+++ environments.py
@@ -1,4 +1,4 @@
 import os
 
-os.environ['KEY_AUTHORIZATION'] = '***'
+os.environ['KEY_AUTHORIZATION'] = '3YY8RC6.nS-1i9E84A1Zy9sq_FyHpAiYzoiavIaqc'
 os.environ['API_CHECKLIST'] = 'https://fleet.cloudfleet.com/api/v1/checklist/?'
